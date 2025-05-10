a7c50b1044f16f0b7ca06f5ca06b214f0de76d12
Sajad <sajadsoorni@gmail.com>
2025-05-10 12:17:20 +0100

    feat: practice number conversion, parsing, and type checking in JS

--- script.js
+++ script.js
@@ -251,3 +251,38 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+console.log(23 === 23.0); // true
+
+// Base 10 - 0 to 9. 1/10 = 0.1. 3/10 =3.3333333
+// Binary base 2 - 0 1
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 + 0.2 === 0.3); // false
+
+// Coversion
+console.log(Number('23')); // 23
+console.log(+'23'); // 23
+
+// Parsing
+console.log(Number.parseInt('30px', 10)); // 30
+console.log(Number.parseInt('e23', 10)); // NaN
+
+console.log(Number.parseInt('  2.5rem  ')); // 2
+console.log(Number.parseFloat('  2.5rem   ')); // 2.5
+
+// console.log(parseFloat('  2.5rem   '));
+
+// Check if value is NaN
+console.log(Number.isNaN(20)); // false
+console.log(Number.isNaN('20')); // false
+console.log(Number.isNaN(+'20X')); // true
+console.log(Number.isNaN(23 / 0)); // false
+
+// Checking if value is number
+console.log(Number.isFinite(20)); // true
+console.log(Number.isFinite('20')); // false
+console.log(Number.isFinite(+'20X')); // false
+console.log(Number.isFinite(23 / 0)); // false
+
+console.log(Number.isInteger(23)); // true
+console.log(Number.isInteger(23.0)); // true
+console.log(Number.isInteger(23 / 0)); // false
